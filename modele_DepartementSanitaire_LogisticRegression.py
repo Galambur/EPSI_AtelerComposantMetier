fb634cdfe9adb38086d38087655f66bc153028ba
Galambur <59735586+Galambur@users.noreply.github.com>
2023-09-12 10:24:57 +0200

    Update modele_DepartementSanitaire_LogisticRegression.py

--- modele_DepartementSanitaire_LogisticRegression.py
+++ modele_DepartementSanitaire_LogisticRegression.py
@@ -29,7 +29,7 @@ data['Synthese_eval_sanit'] = data['Synthese_eval_sanit'].map(synthese_eval_mapp
 X = data[['Departement']]
 y = data['Synthese_eval_sanit']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 # Régression logistique
 model = LogisticRegression(random_state=42)
